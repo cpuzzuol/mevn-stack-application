3df054182a740917fb3cb90bb555eb43f1da4e2f
Chris Puzzuoli <cpuzzuol@umich.edu>
2021-02-09 17:26:28 -0500

    initial

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        {{ customers }}
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        created() {
+            axios.get(this.custURL)
+              .then(r => {
+                console.log(r)
+              })
+              .catch(e => {
+                console.log(e)
+              })
+        },
+        data() {
+            return {
+                custURL: 'http://localhost:3000/Customers',
+                customers: []
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
---
+++ server/db.js
@@ -0,0 +1,19 @@
+/**
+ * for mongodb connection
+ */
+const mongoose = require('mongoose');
+require('dotenv').config();
+const conString = process.env.DB_CONNECTION;
+mongoose.connect(
+  conString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) console.log('Successfully connect to MondoDB...');
+    else
+      console.log(
+        'Connection to MongoDb failed :' + JSON.stringify(err, undefined, 2)
+      );
+  }
+);
+
+module.exports = mongoose;
